3c8ce6b9094272260154b1d68910061ca4108dc9
unknown <younesoumast2@gmail.com>
2023-07-18 22:34:06 +0100

    media queries

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -1,41 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import "../styling/GetInTouch.css"
+import "../styling/GetInTouch.css";
 
 const GetInTouch = () => {
   return (
-    <div id='Get-in-Touch'>
-      <div className='get-in-touch'>
-      <p className='subtext'>Contact</p>
-      <h1 className='title'>Get in touch</h1>
-      <div className='form-and-info'>
-        <form>
-          <div>
-            <input 
-              type="text" 
+    <div id="Get-in-Touch">
+      <div className="get-in-touch">
+        <p className="subtext">Contact</p>
+        <h1 className="title">Get in touch</h1>
+        <div className="form-and-info">
+          <form>
+            <div>
+              <input type="text" name="" placeholder="Name" />
+              <input type="email" name="" placeholder="Email" />
+            </div>
+            <input type="text" name="" placeholder="Subject" />
+            <textarea
+              type="text"
               name=""
-              placeholder='Name'   
+              placeholder="How can we help you"
+              className="message"
             />
-            <input 
-              type="email" 
-              name=""
-              placeholder='Email'   
-            />
-          </div>
-          <input 
-            type="text" 
-            name=""
-            placeholder='Subject'   
-          />
-          <textarea 
-            type="text" 
-            name=""
-            placeholder='How can we help you'   
-            className='message'
-          />
-          <button>Send message</button>
-        </form>
-        <div className='info'>
+            <button>Send message</button>
+          </form>
+          <div className="info">
             <div>
               <h1>Address</h1>
               <p>
@@ -52,11 +40,11 @@ const GetInTouch = () => {
               <h1>Hours</h1>
               <p>Mon-Sat 8am - 5pm</p>
             </div>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default GetInTouch
+export default GetInTouch;
