ec9097642bf8ff3212a3d5dfa2f7b9554f5acf11
unknown <younesoumast2@gmail.com>
2023-07-19 15:55:07 +0100

    added contact module

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, {useRef} from "react";
+import emailjs from '@emailjs/browser';
 
 import "../styling/GetInTouch.css";
 
 const GetInTouch = () => {
+  
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('car_rental', 'template_dwk6rhm', form.current, 'l1_TnfwGwEjpzlB6s')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  
+  
   return (
     <div id="Get-in-Touch">
       <div className="get-in-touch">
         <p className="subtext">Contact</p>
         <h1 className="title">Get in touch</h1>
         <div className="form-and-info">
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <div>
-              <input type="text" name="" placeholder="Name" />
-              <input type="email" name="" placeholder="Email" />
+              <input type="text" name="name" placeholder="Name" />
+              <input type="email" name="email" placeholder="Email" />
             </div>
-            <input type="text" name="" placeholder="Subject" />
+            <input type="text" name="subject" placeholder="Subject" />
             <textarea
               type="text"
-              name=""
+              name="message"
               placeholder="How can we help you"
               className="message"
             />
